c09d866dee8940858ed5c540253ba6817902b6c2
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-10 19:28:28 -0700

    update program.cs config

--- JuneProjClient/Program.cs
+++ JuneProjClient/Program.cs
@@ -7,20 +7,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace JuneProjClient
+namespace JuneClientProj
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+              webBuilder.UseUrls("http://localhost:5004/");
+            });
+  }
 }
